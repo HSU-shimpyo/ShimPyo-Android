ff73c79ed4b80357b59685cb74739c639e0200d9
Juye0nLee <dlwndus708@naver.com>
2024-07-28 16:06:43 +0900

    ✨ 약 설정 버튼 누르면 약 설정 액티비티로 이동

--- app/src/main/java/com/example/shimpyoo/MainActivity.kt
+++ app/src/main/java/com/example/shimpyoo/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.shimpyoo
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.shimpyoo.settings.notification.NotificationActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+
+        val notificationSettingBtn: ImageView = findViewById(R.id.medicineSettingButton)
+        notificationSettingBtn.setOnClickListener {
+            val intent = Intent(applicationContext, NotificationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
