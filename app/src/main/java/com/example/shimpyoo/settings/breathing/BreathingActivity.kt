c9b0fde2e57a30bf7a57df24cd8a34788c870f9a
jungeun-e <lucycy255@gmail.com>
2024-08-02 02:01:12 +0900

    ✨ 두번쨰, 세번째 페이지 추가 구현

--- app/src/main/java/com/example/shimpyoo/settings/breathing/BreathingActivity.kt
+++ app/src/main/java/com/example/shimpyoo/settings/breathing/BreathingActivity.kt
@@ -2,14 +2,13 @@ package com.example.shimpyoo.settings.breathing
 
 import android.os.Bundle
 import android.widget.ImageButton
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.shimpyoo.R
 
 class BreathingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_breathing)
+        setContentView(R.layout.activity_breathing1)
 
         val closeButton: ImageButton = findViewById(R.id.closeButton)
         closeButton.setOnClickListener {
