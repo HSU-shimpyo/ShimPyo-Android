5425e8b960a9b8d4d8a7438dda285a8d81bd2e2a
jungeun-e <lucycy255@gmail.com>
2024-08-02 02:22:07 +0900

    ✨ 세번째 단계까지 화면 이동 구현

--- app/src/main/java/com/example/shimpyoo/settings/breathing/BreathingActivity2.kt
+++ app/src/main/java/com/example/shimpyoo/settings/breathing/BreathingActivity2.kt
@@ -5,22 +5,25 @@ import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.shimpyoo.MainActivity
 import com.example.shimpyoo.R
 
-class BreathingActivity : AppCompatActivity() {
+class BreathingActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_breathing1)
+        setContentView(R.layout.activity_breathing2)
 
         val breathingBtn: ImageView = findViewById(R.id.breathingBtn)
         breathingBtn.setOnClickListener {
-            val intent = Intent(this, BreathingActivity2::class.java)
+            val intent = Intent(this, BreathingActivity3::class.java)
             startActivity(intent)
         }
 
         val closeButton: ImageButton = findViewById(R.id.closeButton)
         closeButton.setOnClickListener {
-            finish()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/shimpyoo/settings/breathing/BreathingActivity3.kt
+++ app/src/main/java/com/example/shimpyoo/settings/breathing/BreathingActivity3.kt
@@ -3,24 +3,20 @@ package com.example.shimpyoo.settings.breathing
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.shimpyoo.MainActivity
 import com.example.shimpyoo.R
 
-class BreathingActivity : AppCompatActivity() {
+class BreathingActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_breathing1)
-
-        val breathingBtn: ImageView = findViewById(R.id.breathingBtn)
-        breathingBtn.setOnClickListener {
-            val intent = Intent(this, BreathingActivity2::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_breathing3)
 
         val closeButton: ImageButton = findViewById(R.id.closeButton)
         closeButton.setOnClickListener {
-            finish()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
         }
     }
 }
