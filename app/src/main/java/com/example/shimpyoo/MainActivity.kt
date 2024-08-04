e66efddd4ee2455e8e9682700696b93472aa4d6f
frombunny <rucy011121@gamil.com>
2024-08-04 14:26:50 +0900

    :sparkles: FEAT. 프래그멘트 추가

--- app/src/main/java/com/example/shimpyoo/MainActivity.kt
+++ app/src/main/java/com/example/shimpyoo/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.shimpyoo
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils.replace
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val button :Button=findViewById(R.id.button_fragment)
+        button.setOnClickListener{
+            val intent=Intent(this, RecordActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
+
+
 }
